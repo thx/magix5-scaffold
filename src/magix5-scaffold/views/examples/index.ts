28c10012929ed64e69c2ddf204bf18a3c25f6d81
言弘 <yanhong.xhj@alibaba-inc.com>
2021-12-23 17:39:50 +0800

    create examples

--- src/magix5-scaffold/views/examples/index.ts
+++ src/magix5-scaffold/views/examples/index.ts
@@ -1,11 +1,27 @@
 import Magix from 'magix5';
 const { View, applyStyle, Router } = Magix;
-
 applyStyle('@:./index.less');
+
+import { exampleInstances as nav } from './config';
 
 export default View.extend({
     tmpl: '@:./index.html',
+    assign(props) {
+        this.set({ props, nav, isChoosed: false });
+    },
     async render() {
         this.digest();
+    },
+    'choose<click>'(e) {
+        const { target } = e.params;
+        Router.to({
+            instance: target,
+        });
+        this.digest({
+            isChoosed: true,
+        });
+    },
+    'back<click>'() {
+        Router.to('/home');
     },
 });
